e2c1f05170bf54fc887dda2d96d9e74a1a2fb1dc
nicolas-f <github@nettrader.fr>
2025-06-09 12:30:15 +0200

    set final menu

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,94 +1,55 @@
 import { useState } from 'react';
 import {
-    IconCalendarStats,
-    IconDeviceDesktopAnalytics,
-    IconFingerprint,
-    IconGauge,
-    IconHome2,
-    IconSettings,
-    IconUser,
+  IconBuildingFactory2,
+  IconDatabase,
+  IconDeviceDesktopAnalytics,
+  IconGraph,
+  IconSettings,
+  IconSwitchHorizontal,
 } from '@tabler/icons-react';
-import { Title, Tooltip, UnstyledButton } from '@mantine/core';
+import { Group } from '@mantine/core';
 import classes from './NavigationBar.module.css';
 
 const mainLinksMockdata = [
-    { icon: IconHome2, label: 'Home' },
-    { icon: IconGauge, label: 'Dashboard' },
-    { icon: IconDeviceDesktopAnalytics, label: 'Analytics' },
-    { icon: IconCalendarStats, label: 'Releases' },
-    { icon: IconUser, label: 'Account' },
-    { icon: IconFingerprint, label: 'Security' },
-    { icon: IconSettings, label: 'Settings' },
-];
-
-const linksMockdata = [
-    'Security',
-    'Settings',
-    'Dashboard',
-    'Releases',
-    'Account',
-    'Orders',
-    'Clients',
-    'Databases',
-    'Pull Requests',
-    'Open Issues',
-    'Wiki pages',
+  { link: '', icon: IconSettings, label: 'Settings' },
+  { link: '', icon: IconDatabase, label: 'Game & Save Data' },
+  { link: '', icon: IconBuildingFactory2, label: 'Production' },
+  { link: '', icon: IconGraph, label: 'Game statistics' },
 ];
 
 export function NavigationBar() {
-    const [active, setActive] = useState('Releases');
-    const [activeLink, setActiveLink] = useState('Settings');
+  const [active, setActive] = useState('Settings');
 
-    const mainLinks = mainLinksMockdata.map((link) => (
-        <Tooltip
-            label={link.label}
-            position="right"
-            withArrow
-            transitionProps={{ duration: 0 }}
-            key={link.label}
-        >
-            <UnstyledButton
-                onClick={() => setActive(link.label)}
-                className={classes.mainLink}
-                data-active={link.label === active || undefined}
-            >
-                <link.icon size={22} stroke={1.5} />
-            </UnstyledButton>
-        </Tooltip>
-    ));
+  const mainLinks = mainLinksMockdata.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
 
-    const links = linksMockdata.map((link) => (
-        <a
-            className={classes.link}
-            data-active={activeLink === link || undefined}
-            href="#"
-            onClick={(event) => {
-                event.preventDefault();
-                setActiveLink(link);
-            }}
-            key={link}
-        >
-            {link}
-        </a>
-    ));
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        <Group className={classes.header} justify="space-between">
+          <div className={classes.logo}>
+            <img src="src/favicon.png" alt="logo" />
+          </div>
+        </Group>
+        {mainLinks}
+      </div>
 
-    return (
-        <nav className={classes.navbar}>
-            <div className={classes.wrapper}>
-                <div className={classes.aside}>
-                    <div className={classes.logo}>
-                        <img src="src/favicon.png" alt="logo" />
-                    </div>
-                    {mainLinks}
-                </div>
-                <div className={classes.main}>
-                    <Title order={4} className={classes.title}>
-                        {active}
-                    </Title>
-
-                    {links}
-                </div>
-            </div>
-        </nav>
-    );
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()} />
+      </div>
+    </nav>
+  );
 }
--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,9 +1,51 @@
-import {NavigationBar} from "@/components/NavigationBar/NavigationBar";
+import * as fs from 'node:fs';
+import { Button, Flex } from '@mantine/core';
+import { NavigationBar } from "@/components/NavigationBar/NavigationBar";
+// import gameData from '../data/media_soviet.zip';
+
+const loadGameAssetWorker = new Worker(new URL("/worker/loadGameAsset.js", import.meta.url));
+
+async function parseZipFile() {
+  loadGameAssetWorker.postMessage("test")
+  //const filePath : fs.PathLike= path.resolve(__dirname, 'data/media_soviet.zip');
+  // const readStream = fs.createReadStream(gameData, { encoding: 'utf8' });
+  //
+  // try {
+  //   for await (const chunk of readStre
+  //   am) {
+  //     console.log('--- File chunk start ---');
+  //     console.log(chunk);
+  //     console.log('--- File chunk end ---');
+  //   }
+  //   console.log('Finished reading the file.');
+  // } catch (error) {
+  //   console.error(error);
+  // }
+  //   console.error(`Error reading file: ${error.message}`);
+  // }
+}
+
+function HomeContent() {
+  return (
+    <Flex
+      mih={50}
+      bg="rgba(0, 0, 0, .3)"
+      gap="sm"
+      justify="center"
+      align="center"
+      direction="column"
+      wrap="nowrap"
+    >
+      <Button onClick={parseZipFile} >Load game data</Button>
+    </Flex>
+  );
+}
 
 export function HomePage() {
   return (
     <>
       <NavigationBar />
+      <HomeContent />
     </>
   );
 }
