8f5b60c207f109ebe6c3aa1965978c8fa3c4cd37
nicolas-f <github@nettrader.fr>
2025-06-09 09:51:11 +0200

    config app path

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    // Add other routes here...
+  ],
   {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
+    basename: '/war_plans_dashboard', // Add basename here
+  });
 
 export function Router() {
   return <RouterProvider router={router} />;
