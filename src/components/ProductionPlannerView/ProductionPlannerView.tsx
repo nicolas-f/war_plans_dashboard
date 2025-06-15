fe1da2763e7e3fd7a6dfeaf81c739e96259ea64a
nicolas-f <github@nettrader.fr>
2025-06-15 23:02:40 +0200

    responsive controls

--- src/components/ProductionPlannerView/ProductionPlannerView.tsx
+++ src/components/ProductionPlannerView/ProductionPlannerView.tsx
@@ -1,3 +1,4 @@
+;
 /*
  * Copyright (C) 2025 -  Nicolas Fortin - https://github.com/nicolas-f
  *
@@ -14,15 +15,16 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+import dayjs from 'dayjs';
 import React, { useState } from 'react';
-import { IconCross, IconRowRemove } from '@tabler/icons-react';
-import { ActionIcon, Box, NumberInput, Select, Slider, Stack, Table, TableData, Group } from '@mantine/core';
+import { IconRowRemove } from '@tabler/icons-react';
+import { ActionIcon, Group, NumberInput, Select, Stack, Table, TableData } from '@mantine/core';
 import { ComboboxItem } from '@mantine/core/lib/components/Combobox/Combobox.types';
+import { DatePickerInput } from '@mantine/dates';
 import { Entity } from '@/database/entity';
 import { GameDatabase } from '@/database/gameDatabase';
 import { SaveGameDatabase } from '@/database/saveGameDatabase';
-import { DatePickerInput } from '@mantine/dates';
-import dayjs from 'dayjs';
+
 
 export interface ProductionGameContentProps {
   gameDatabase: GameDatabase;
@@ -33,7 +35,7 @@ export interface ProductionGameContentProps {
 
 class ProductionTableRow {
   building: string;
-  productivity: number;
+  productivity: string | number;
   quantity: string | number;
   constructor(building : string) {
     this.building = building;
@@ -51,15 +53,16 @@ function getResourcesProduction(gameDatabase: GameDatabase, saveGameDatabase: Sa
   // fetch all production and consumption numbers for all resources and all listed buildings
   buildings.forEach((element) => {
     const buildingEntity = gameDatabase.entities.get(element.building)
-    if (typeof element.quantity === 'number') {
+    if (typeof element.quantity === 'number' && typeof element.productivity === 'number') {
       const numberOfBuildings: number = element.quantity;
+      const productivityBuildings: number = element.productivity;
       if (buildingEntity) {
         buildingEntity.getProduction(productionDate.year()).forEach(({resource, quantity}) => {
-          const totalQuantity = quantity * numberOfBuildings * ( buildingEntity.getMaximumWorkers() * element.productivity / 100);
+          const totalQuantity = quantity * numberOfBuildings * ( buildingEntity.getMaximumWorkers() * productivityBuildings / 100);
           resources.set(resource, (resources.get(resource) || 0) + totalQuantity)
         })
         buildingEntity.getConsumption(productionDate.year()).forEach(({resource, quantity}) => {
-          const totalQuantity = quantity * ( buildingEntity.getMaximumWorkers() * element.productivity / 100);
+          const totalQuantity = quantity * ( buildingEntity.getMaximumWorkers() *productivityBuildings / 100);
           resources.set(resource, (resources.get(resource) || 0) - totalQuantity)
         })
       }
@@ -86,31 +89,40 @@ export function ProductionGameContent({
 
   // Generate building search data
   const buildingSelectionData: { group: string; items: ComboboxItem[] }[] = [];
-  gameDatabase.entities.entries()
-    .filter(value => value[1].getLocalizedNameIndex() > 0 && (value[1].getMaximumWorkers() > 0 || value[1].getLivingPopulation() > 0))
+  gameDatabase.entities
+    .entries()
+    .filter(
+      (value) =>
+        value[1].getLocalizedNameIndex() > 0 &&
+        (value[1].getMaximumWorkers() > 0 || value[1].getLivingPopulation() > 0)
+    )
     .forEach((value: [string, Entity]) => {
-    const group = value[1].getType();
-    const maxWorker = value[1].getMaximumWorkers();
-    const maxPopulation = value[1].getLivingPopulation();
-    const label = maxWorker > 0 ? `${gameDatabase.getLang(selectedLanguage, value[1].getLocalizedNameIndex())}
-     - ${maxWorker} ${gameDatabase.getLang(selectedLanguage, 585)}` : `${gameDatabase.getLang(selectedLanguage, value[1].getLocalizedNameIndex())}
-     - ${maxPopulation} ${gameDatabase.getLang(selectedLanguage, 2810)}`
-    const comboboxItem : ComboboxItem = {value: value[1].name,
-      label}
-    const index = buildingSelectionData.findIndex((e) => e.group === group)
-    if( index === -1) {
-      buildingSelectionData.push({group, items:[comboboxItem]})
-    } else {
-      buildingSelectionData[index].items.push(comboboxItem)
-    }
-  })
+      const group = value[1].getType();
+      const maxWorker = value[1].getMaximumWorkers();
+      const maxPopulation = value[1].getLivingPopulation();
+      const label =
+        maxWorker > 0
+          ? `${gameDatabase.getLang(selectedLanguage, value[1].getLocalizedNameIndex())}
+     - ${maxWorker} ${gameDatabase.getLang(selectedLanguage, 585)}`
+          : `${gameDatabase.getLang(selectedLanguage, value[1].getLocalizedNameIndex())}
+     - ${maxPopulation} ${gameDatabase.getLang(selectedLanguage, 2810)}`;
+      const comboboxItem: ComboboxItem = { value: value[1].name, label };
+      const index = buildingSelectionData.findIndex((e) => e.group === group);
+      if (index === -1) {
+        buildingSelectionData.push({ group, items: [comboboxItem] });
+      } else {
+        buildingSelectionData[index].items.push(comboboxItem);
+      }
+    });
 
   const buildingsTableData: TableData = {
     caption: gameDatabase.getLang(selectedLanguage, 59008),
-    head: ["",
+    head: [
+      '',
       gameDatabase.getLang(selectedLanguage, 13900),
       gameDatabase.getLang(selectedLanguage, 8090),
-      'Quantity'],
+      gameDatabase.getLang(selectedLanguage,8068)
+    ],
     body: buildings.map((element) => [
       <ActionIcon
         variant="filled"
@@ -119,24 +131,34 @@ export function ProductionGameContent({
         <IconRowRemove />
       </ActionIcon>,
       gameDatabase.entities.get(element.building)!.getLocalizedNameIndex() > 0
-        ? gameDatabase.getLang(selectedLanguage, gameDatabase.entities.get(element.building)!.getLocalizedNameIndex())
+        ? gameDatabase.getLang(
+            selectedLanguage,
+            gameDatabase.entities.get(element.building)!.getLocalizedNameIndex()
+          )
         : gameDatabase.entities.get(element.building)!.name,
-      <Slider
-        w="120px"
-        min={0}
+      <NumberInput
+        min={10}
         max={100}
-        defaultValue={100}
+        suffix=" %"
         step={10}
-        onChange={(v) => {
-          setBuildings(buildings.map((a) => a.building === element.building ? {...a, productivity: v} : a))
+        w="80px"
+        value={element.productivity}
+        onChange={(productivity) => {
+          setBuildings(
+            buildings.map((a) => (a.building === element.building ? { ...a, productivity} : a))
+          );
         }}
-        marks={[
-          { value: 0, label: '0%' },
-          { value: 50, label: '50%' },
-          { value: 100, label: '100%' },
-        ]}
       />,
-      <NumberInput min={1} w="80px" defaultValue={1} onChange={(v) => {setBuildings(buildings.map((a) => a.building === element.building ? {...a, quantity: v} : a))}} />,
+      <NumberInput
+        min={1}
+        w="80px"
+        value={element.quantity}
+        onChange={(quantity) => {
+          setBuildings(
+            buildings.map((a) => (a.building === element.building ? { ...a, quantity } : a))
+          );
+        }}
+      />,
     ]),
   };
 
